77b32d802812f62972a9ee99e1995f27fec709eb
Gabriel Lesperance <gabriel@wavo.me>
2013-10-16 00:46:26 -0400

    ADDED - New Parallax Effect

--- js/parallax.js
+++ js/parallax.js
@@ -1,37 +1,79 @@
-$(document).ready(function(){   
+$(document).ready(function (){   
   // Cache the Window object
   $window = $(window);
 
+    // Example 1
+    // || |--------------|
+    // || |    FALSE     |
+    // || |              |
+    // || |--------------|
+    // ||=================== <--------------- windowTop
+    // || |--------------| <--- elementRect.top
+    // || |    TRUE      |
+    // || |              |
+    // || |--------------| <--- elementRect.bottom
+    // ||=================== <--------------- windowBottom
+
+    // Example 2
+    // || |--------------| 
+    // || |    FALSE     |
+    // || |              |
+    // || |--------------|
+    // || |--------------| <--- elementRect.top
+    // || |    TRUE      |
+    // ||=|==============|== <--------------- windowTop
+    // || |--------------| <--- elementRect.bottom
+    // ||
+    // || |--------------| <--- elementRect.top
+    // || |    TRUE      |
+    // ||=|==============|== <--------------- windowBottom
+    // || |--------------| <--- elementRect.bottom
+    // ||
+    // || |--------------| 
+    // || |    FALSE     |
+    // || |              |
+    // || |--------------| 
+
+
   // For each element that has a data-type attribute
-  $('[data-type="background"]').each(function(){
+  $('[data-type="background"]').each(function (){
 
-          // Store some variables based on where we are
-          var $self = $(this),
-                  offsetCoords = $self.offset(),
-                  topOffset = offsetCoords.top;
+    // Store some variables based on where we are
+    var self        = this
+      , $self       = $(self)
+      ;
 
-          // When the window is scrolled...
-      $(window).scroll(function() {
+      var speed = +($self.attr('data-speed') || 1)
+      var windowHeight, selfHeight
 
-                  // If this section is in view
-                  if ( ($window.scrollTop() + $window.height()) > (topOffset) &&
-                           ( (topOffset + $self.height()) > $window.scrollTop() ) ) {
+      function onResize() {
+        windowHeight = $window.height()
+        selfHeight  = $self.height() 
+      }
 
-                          // Scroll the background at var speed
-                          // the yPos is a negative value because we're scrolling it UP!                              
-                          var yPos = -($window.scrollTop() / $self.data('speed')); 
+      $(window).resize(onResize)
 
-                          // Put together our final background position
-                          var coords = '50% '+ yPos + 'px';
+      // Hacks for Firefox not having the element sized properly after load
+      // This is perhaps due to LESS beign compiled on-the-fly... 
+      // this should be investigated further.
+      setTimeout(onResize, 0);
+      setTimeout(onResize, 100);
 
-                          // Move the background
-                          $self.css({ backgroundPosition: coords });
+      // When the window is scrolled...
+      $(window).scroll(function () {
+        var elementRect = self.getBoundingClientRect()
+        
+        var elementTopIsinView = elementRect.top >= 0 && elementRect.top < windowHeight
+        var elementBottomIsInView = elementRect.bottom >= 0 && elementRect.bottom < windowHeight
 
+        // We want the background to go DOWN.
+        // ==> we want background top position to augment in a fashion
+        // that is inversely proportional to the rect top
+        if (elementBottomIsInView)
+          $self.css('background-position-y', -selfHeight - elementRect.top * speed + 'px')
 
-                  }; // in view
-
-          }); // window scroll
-
-  }); // each data-type
-
-}); // document ready
+        else if (elementTopIsinView)
+          $self.css('background-position-y', -selfHeight + elementRect.top * speed + 'px')
+    })
+  })
+})
