da1f159591e404bf097b2de8087a7e04d6544011
Gabriel Lesperance <gabriel@wavo.me>
2013-10-22 02:11:13 -0400

    Weird state

--- config/connect.php
+++ config/connect.php
@@ -2,5 +2,5 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 define('_MYSQL_SET_SQL_MODE',true);
 $GLOBALS['spip_connect_version'] = 0.7;
-spip_connect_db('localhost','','root','root','spip_1700','mysql', 'spip','');
+spip_connect_db('localhost','','','','spip_1700','sqlite3', 'spip','');
 ?>
